959ed8d8dacdac5e39f4d18665ba6f2b10661713
kunal05091999 <116063387+kunal05091999@users.noreply.github.com>
2024-02-08 18:38:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function validateForm(){
     }
 
     var email = document.forms['myForm']["femail"].value;
-    if (email.length>15){
+    if (email.length>28){
         seterror("email", "*Email length is too long");
         returnval = false;
     }
